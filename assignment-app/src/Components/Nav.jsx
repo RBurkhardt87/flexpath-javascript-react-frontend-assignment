212bfe702b1725723a7ab3f2d352fc63ad3225e2
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-07-01 16:53:30 -0500

    Nav bar works and looks like navbar in the demo

--- assignment-app/src/Components/Nav.jsx
+++ assignment-app/src/Components/Nav.jsx
@@ -4,10 +4,22 @@ const Nav = () => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
-            <p className="navbar-brand ms-4 nav-link">User Behavior Data</p>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">User Behavior Data</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                            <a className="nav-link" href="/search">Search</a>
+                            </li>
+                        </ul>
+                    </div> 
+            </div>          
         </nav>
     </div>
+    
   )
 }
 
